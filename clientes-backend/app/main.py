a9dc7abe183457fd89a8564895660c3142b311b7
AriekP <160274308+AriekP@users.noreply.github.com>
2025-05-12 19:54:47 -0400

    Front mejorado

--- clientes-backend/app/main.py
+++ clientes-backend/app/main.py
@@ -3,8 +3,20 @@ from fastapi import FastAPI
 from app.database import Base, engine
 from app.models import cliente
 from app.routers import clientes
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+
+# ✅ Middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # o ["*"] si estás en desarrollo
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Crear tablas
 Base.metadata.create_all(bind=engine)
